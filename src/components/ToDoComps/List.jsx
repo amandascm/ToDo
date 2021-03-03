97e8566b9cf442b72d5d0d8861df55136b2b98cc
Amanda <amandascm23@gmail.com>
2021-03-03 12:59:35 -0300

    [ToDoList] Modal edition basis (fail update)

--- src/components/ToDoComps/List.jsx
+++ src/components/ToDoComps/List.jsx
@@ -1,13 +1,33 @@
 import { React, useState } from 'react';
+import { Form } from 'react-bootstrap';
 import ListItem from './ListItem';
-import Modal from '../Modal';
+import ModalComponent from '../Modal';
 
 export default function ToDoList({ taskList, changeList }) {
   const [showModal, setShowModal] = useState(false);
   const [editionTask, setEditionTask] = useState({});
+  const [newTaskName, setNewTaskName] = useState('');
+
+  const updateNewTaskName = ({ target: { value } }) => {
+    setNewTaskName(value);
+  };
+
+  const onSubmit = () => {
+    changeList(taskList.map((item) => {
+      if (item.id === editionTask.id) {
+        return {
+          ...item,
+          name: newTaskName,
+        };
+      }
+      return item;
+    }));
+    setShowModal(!showModal);
+  };
 
   const onEdit = (task) => {
     setEditionTask(task);
+    setNewTaskName(task.name);
     setShowModal(!showModal);
   };
 
@@ -21,7 +41,9 @@ export default function ToDoList({ taskList, changeList }) {
           <ListItem key={item.id} item={item} taskList={taskList} changeList={changeList} onEdit={onEdit} />
         ))}
       </div>
-      <Modal show={showModal} toggle={onClose} title={editionTask.name} />
+      <ModalComponent show={showModal} toggle={onClose} onSubmit={onSubmit} title={editionTask.name}>
+        <Form.Control maxLength="80" value={newTaskName} onChange={updateNewTaskName} type="text" />
+      </ModalComponent>
     </>
   );
 }
